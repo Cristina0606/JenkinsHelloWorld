b7b633d5cce875f7f0f6eec0dc4b3fd4021a7132
cristina sanz <cristina.sanz@solera.com>
2023-06-20 11:52:01 +0200

    Adding new files

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.sanzporras;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main (String [] args ) throws IOException {
+
+
+
+        System.out.println("dime el valor de la comunidad y piso");
+
+    }
+}
